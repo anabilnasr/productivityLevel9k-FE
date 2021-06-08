5ca961747062d968e3a75f71e9cf57a1765238af
xNebulaa <ah.nabilnasr@gmail.com>
2021-06-08 08:42:27 +0200

    user card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import altphoto from '../assets/dummy-user.png'
 import { Container } from 'react-bootstrap'
+import './UserCard.css'
 export default function UserCard(props) {
 
     return (
         <Container>
-            <img style={{ borderRadius: "50%" }} src={props.userData.photoURL ? props.userData.photoURL.replace('=s96', '=s150') : altphoto} alt="UserPhoto" />
+            <img className="round" src={props.userData.photoURL ? props.userData.photoURL.replace('=s96', '=s150') : altphoto} alt="UserPhoto" />
             <h1>{props.userData.name}</h1>
             <button onClick={props.logout}>Logout</button>
         </Container>
